8719c16017bb2c5f2c173be7ce8d33a092870bc0
mutohayate <157664291+mutohayate@users.noreply.github.com>
2026-02-08 13:49:46 +0900

    update module alb

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -11,6 +11,16 @@ resource "aws_lb_target_group" "this" {
   protocol     = "HTTP"
   vpc_id       = var.vpc_id
   target_type  = "ip"
+
+    health_check {
+    path                = "/"   
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_listener" "http" {
